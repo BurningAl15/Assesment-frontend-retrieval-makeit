6093a2ed2546369835bbeb611c053ab24cfc5ebe
Aldhair Vera Camacho <aldhairvera15@gmail.com>
2022-02-13 15:17:48 -0500

    fix: eslint warnings cleared

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import useProductDetails from '../../hooks/useProductDetails';
 import { PageContainer, FetchDataContainer } from '../styles/style';
