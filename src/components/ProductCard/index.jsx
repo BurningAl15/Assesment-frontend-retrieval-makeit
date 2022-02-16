45e07b00e3d794a2e495048226fd238a6017e223
Aldhair Vera Camacho <acamacho@globaltask.net>
2022-02-16 09:12:23 -0500

    fix: eslint error solved

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 import { useNavigate } from 'react-router-dom';
